7f41296f2ec64986fb5d5b56bc859413136decae
ridwanray <alabarise@gmail.com>
2021-08-09 15:53:30 +0100

    react pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Pagination from './Pagination';
+import Post from './Post';
+const url = 'https://jsonplaceholder.typicode.com/posts';
 
-function App() {
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw new Error('something went wrong while requesting posts');
+      })
+      .then((posts) => setPosts(posts))
+      .catch((error) => setError(error.message));
+  }, []);
+
+  if (error) return <h1>{error}</h1>;
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {posts.length > 0 ? (
+        <>
+     
+          <Pagination
+            data={posts}
+            RenderComponent={Post}
+            title="Posts"
+            pageLimit={4}
+            dataLimit={10}
+          />
+        </>
+      ) : (
+       <h1>No Posts to display</h1>
+      )}
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/Post.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Post(props) {
+    const { id, title, body } = props.data;
+    return (
+      <div className="post">
+        <small>{id}</small>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    );
+  }
